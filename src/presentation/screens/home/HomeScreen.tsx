22ac318cd524600a97a8e0e1493ffc8f9e73b611
Nestor Bardel <nestorbardel@gmail.com>
2024-12-02 00:22:27 -0300

    Empezando MovieApp

---
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -0,0 +1,13 @@
+//import liraries
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// create a component
+export const HomeScreen = () => {
+    return (
+        <View>
+            <Text>HomeScreen</Text>
+        </View>
+    );
+};
+
