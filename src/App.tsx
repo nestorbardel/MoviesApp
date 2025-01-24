cadb993a7753b83452de5520a6cb991a5200c67c
Nestor Bardel <nestorbardel@gmail.com>
2025-01-24 14:43:55 -0300

    144 - terminado

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import '../gesture-handler';
 import 'react-native-gesture-handler';
-import { View, Text } from 'react-native';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { Navigation } from './presentation/navigation/Navigation';
 
 export default function App() {
   return (
         <NavigationContainer>
-            <View>
-                <Text>MoviesApp</Text>
-            </View>
+            <Navigation />
         </NavigationContainer>
   );
 }
