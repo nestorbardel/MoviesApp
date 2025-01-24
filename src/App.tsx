681990a8845073c99d2d316a041ad2b65c34fad9
Nestor Bardel <nestorbardel@gmail.com>
2025-01-24 10:46:40 -0300

    Agregadas importaciones de stack

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import '../gesture-handler';
+import 'react-native-gesture-handler';
 import { View, Text } from 'react-native';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View>
-      <Text>MoviesApp</Text>
-    </View>
+        <NavigationContainer>
+            <View>
+                <Text>MoviesApp</Text>
+            </View>
+        </NavigationContainer>
   );
 }
