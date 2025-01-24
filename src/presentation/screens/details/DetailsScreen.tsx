667dd3483a12aa2e07bedc14124cd3b845e8725c
Nestor Bardel <nestorbardel@gmail.com>
2025-01-24 04:47:49 -0300

    cambios DetailsScreen

--- src/presentation/screens/details/DetailsScreen.tsx
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 
-export default function DetailsScreen() {
+export const DetailsScreen = () => {
   return (
     <View>
       <Text>DetailsScreen</Text>
